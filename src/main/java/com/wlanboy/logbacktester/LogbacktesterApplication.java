ab67372a97c169ff9c0901a80587a9ef17dbc311
wlanboy <me@wlanboy.com>
2018-11-17 16:26:20 +0100

    Added mvn and project files.

---
+++ src/main/java/com/wlanboy/logbacktester/LogbacktesterApplication.java
@@ -0,0 +1,12 @@
+package com.wlanboy.logbacktester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbacktesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbacktesterApplication.class, args);
+	}
+}
